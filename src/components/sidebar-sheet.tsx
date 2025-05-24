bce688ddf9431ac489587e28345e877426ce8c85
Andre <andredevs1@gmail.com>
2025-05-24 20:15:50 -0300

    chore: add next auth setup

--- src/components/sidebar-sheet.tsx
+++ src/components/sidebar-sheet.tsx
@@ -1,10 +1,18 @@
-import { Calendar1Icon, HomeIcon, LogOutIcon } from 'lucide-react'
+import { Calendar1Icon, HomeIcon, LogInIcon, LogOutIcon } from 'lucide-react'
 import { Button } from './ui/button'
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from './ui/sheet'
 import { Avatar, AvatarImage } from './ui/avatar'
 import Link from 'next/link'
 import { quickSearchOptions } from '@/app/_constants/search'
 import { Icon } from './icons'
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+  DialogTitle,
+  DialogDescription,
+} from './ui/dialog'
 
 export function SidebarSheet() {
   return (
@@ -13,20 +21,40 @@ export function SidebarSheet() {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google.
+              </DialogDescription>
+            </DialogHeader>
+            <Button className="font-bold" variant="outline">
+              <Icon name="Google" />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
           <AvatarImage src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         </Avatar>
 
         <div>
           <p className="font-bold">Felipe Rocha</p>
           <p className="text-xs">felipe@teste.com.br</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
         <SheetClose asChild>
-          <Button className="justify-start gap-2" asChild>
+          <Button variant="ghost" className="justify-start gap-2" asChild>
             <Link href="/">
               <HomeIcon size={18} />
               Inicio
